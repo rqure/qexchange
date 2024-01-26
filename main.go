66789739732d12884ec2cb015e0fbfbb4456d095
rqure <64617806+rqure@users.noreply.github.com>
2024-01-26 18:38:48 +0000

    some fixes

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
-	"log"
+	"fmt"
 	"os"
 	"os/signal"
 	"strconv"
+	"sync"
 	"time"
 
 	qmq "github.com/rqure/qmq/src"
@@ -16,15 +18,22 @@ type ProducerConfig struct {
 	Length int64
 }
 
-func ParseExchangeMap() map[string][]ProducerConfig {
+func ParseExchangeMap(configFile string, logger *qmq.QMQLogger) map[string][]ProducerConfig {
 	var result map[string][][2]interface{}
-	err := json.Unmarshal([]byte(jsonStr), &result)
+	structuredResult := make(map[string][]ProducerConfig)
+
+	content, err := os.ReadFile(configFile)
 	if err != nil {
-		log.Fatalf("Error parsing JSON: %s", err)
+		logger.Error(fmt.Sprintf("Error reading file: %s", err))
+		return structuredResult
 	}
 
-	// Convert to more structured data
-	structuredResult := make(map[string][]ProducerConfig)
+	err = json.Unmarshal(content, &result)
+	if err != nil {
+		logger.Error(fmt.Sprintf("Error parsing JSON: %s", err))
+		return structuredResult
+	}
+
 	for key, value := range result {
 		for _, v := range value {
 			queueName, ok := v[0].(string)
@@ -44,30 +53,59 @@ func ParseExchangeMap() map[string][]ProducerConfig {
 	return structuredResult
 }
 
-func main() {
-	app := qmq.NewQMQApplication("exchange")
-	app.Initialize()
-	defer app.Deinitialize()
-
-	key := os.Getenv("APP_NAME") + ":logs"
-	app.AddConsumer(key).Initialize()
-	app.Consumer(key).ResetLastId()
-
+func MainLoop(ctx context.Context, consumer string, producerConfig []ProducerConfig, app *qmq.QMQApplication) {
 	tickRateMs, err := strconv.Atoi(os.Getenv("TICK_RATE_MS"))
 	if err != nil {
 		tickRateMs = 100
 	}
 
-	sigint := make(chan os.Signal, 1)
-	signal.Notify(sigint, os.Interrupt)
-
 	ticker := time.NewTicker(time.Duration(tickRateMs) * time.Millisecond)
 	for {
 		select {
-		case <-sigint:
+		case <-ctx.Done():
 			return
 		case <-ticker.C:
+			for {
+				popped := app.Consumer(consumer).Pop()
+				if popped == nil {
+					break
+				}
 
+				popped.Ack()
+			}
 		}
 	}
+}
+
+func main() {
+	app := qmq.NewQMQApplication("exchange")
+	app.Initialize()
+	defer app.Deinitialize()
+
+	sigint := make(chan os.Signal, 1)
+	signal.Notify(sigint, os.Interrupt)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	var wg sync.WaitGroup
+
+	exchangeMap := ParseExchangeMap("exchanges.json", app.Logger())
+	for consumer, producers := range exchangeMap {
+		app.AddConsumer(consumer).Initialize()
+
+		for _, producerConfig := range producers {
+			app.AddProducer(producerConfig.Queue).Initialize(producerConfig.Length)
+		}
+
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			MainLoop(ctx, consumer, producers, app)
+		}()
+	}
+
+	<-sigint
+	app.Logger().Advise("SIGINT received")
+
+	cancel()
+	wg.Wait()
 }
