0793403468d0838009f760d9bf15281852b85812
rqure <64617806+rqure@users.noreply.github.com>
2024-01-25 17:34:43 -0700

    Update main.go

--- main.go
+++ main.go
@@ -11,6 +11,39 @@ import (
 	qmq "github.com/rqure/qmq/src"
 )
 
+type ProducerConfig struct {
+	Queue string
+	Length int64
+}
+
+func ParseExchangeMap() map[string][]ProducerConfig {
+	var result map[string][][2]interface{}
+	err := json.Unmarshal([]byte(jsonStr), &result)
+	if err != nil {
+		log.Fatalf("Error parsing JSON: %s", err)
+	}
+	
+	// Convert to more structured data
+	structuredResult := make(map[string][]ProducerConfig)
+	for key, value := range result {
+		for _, v := range value {
+			queueName, ok := v[0].(string)
+			if !ok {
+				continue
+			}
+			
+			number, ok := v[1].(float64) // JSON numbers are floats
+			if !ok {
+				continue
+			}
+			
+			structuredResult[key] = append(structuredResult[key], ProducerConfig{Queue: queueName, Length: int64(number)})
+		}
+	}
+
+	return structuredResult
+}
+
 func main() {
 	app := qmq.NewQMQApplication("exchange")
 	app.Initialize()
